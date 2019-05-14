48b5e08b4418df39f6f3f3ed687c8f380233b949
Aurélien <lebreton97480@gmail.com>
2019-05-14 22:01:04 +0200

    Init

---
+++ Assets/InputSetting.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class InputSetting
+{
+    Dictionary<string, KeyCode> Inputs;
+
+    private static InputSetting Instance;
+    private Array ListKeyCode = Enum.GetValues(typeof(KeyCode));
+
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        Debug.Log("OnRuntimeMethodLoad");
+        Instance = new InputSetting();
+    }
+
+    bool LoadSave = false;
+    public InputSetting()
+    {
+        Inputs = new Dictionary<string, KeyCode>();
+        string save = PlayerPrefs.GetString("InputSetting");
+
+        if (save.Length > 0)
+        {
+            save = save.Remove(0, 1);
+            Debug.Log(save);
+            string[] lines = save.Split(new string[] { "\n" }, StringSplitOptions.None);
+            foreach (string line in lines)
+            {
+                if (line.Length > 0)
+                {
+                    string[] data = line.Split('-');
+                    Inputs.Add(data[0], (KeyCode)ListKeyCode.GetValue(int.Parse(data[1])));
+                }
+            }
+
+            LoadSave = true;
+        }
+        else
+            LoadSave = false;
+    }
+
+    public static bool IsLoadSave()
+    {
+        return Instance.Interne_IsLoadSave();
+    }
+    private bool Interne_IsLoadSave()
+    {
+        return LoadSave;
+    }
+
+    public static void SaveInput()
+    {
+        Instance.Interne_SaveInput();
+    }
+
+    private void Interne_SaveInput()
+    {
+        string data = "$";
+        foreach (KeyValuePair<string, KeyCode> entry in Inputs)
+        {
+            Debug.Log(entry.Key);
+            data += entry.Key + "-" + (int)entry.Value + "\n";
+        }
+
+        Debug.Log(data);
+        PlayerPrefs.SetString("InputSetting", data);
+    }
+
+    public static KeyCode GetKey(string name)
+    {
+      return Instance.Interne_GetKey(name);
+    }
+
+    private KeyCode Interne_GetKey(string name)
+    {
+        return Inputs[name];
+    }
+
+    public static void SetKey(string name, KeyCode key)
+    {
+        Instance.Interne_SetKey(name, key);
+    }
+
+    private void Interne_SetKey(string name, KeyCode key)
+    {
+        if (Inputs.ContainsKey(name))
+            Inputs[name] = key;
+        else
+            Inputs.Add(name, key);
+    }
+}
---
+++ Assets/UIInputSetting.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInputSetting : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Panel;
+
+    void Start()
+    {
+        if (InputSetting.IsLoadSave())
+        {
+            InputSetting.SetKey("forward", KeyCode.Z);
+            InputSetting.SetKey("backward", KeyCode.S);
+            InputSetting.SetKey("rightward", KeyCode.D);
+            InputSetting.SetKey("leftward", KeyCode.Q);
+        }
+        Panel.SetActive(false);
+    }
+
+    string key_Input_Name;
+    Text Value_Input_Name;
+
+    public void XSetInput(GameObject obj)
+    {
+        Value_Input_Name = obj.GetComponent<Text>();
+    }
+
+    public void SetInput(string name)
+    {
+        Panel.SetActive(true);
+        key_Input_Name = name;
+    }
+
+    public void SaveInput()
+    {
+        InputSetting.SaveInput();
+    }
+
+    void LateUpdate()
+    {
+        if (Panel.activeSelf)
+        {
+            Array all = Enum.GetValues(typeof(KeyCode));
+            foreach (KeyCode e in all)
+            {
+                if (Input.GetKeyDown(e))
+                {
+                    InputSetting.SetKey(key_Input_Name, e);
+                    Value_Input_Name.text = e.ToString();
+                    Panel.SetActive(false);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(InputSetting.GetKey("forward"))){
+            Debug.Log("J'avance !");
+        }
+    }
+}
